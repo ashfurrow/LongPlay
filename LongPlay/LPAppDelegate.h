1c1e0aa21dfd27082bd7ee2b0ecf343196230d18
Ash Furrow <ash@500px.com>
2013-01-27 08:52:01 -0500

    Added design files.

---
+++ LongPlay/LPAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AFAppDelegate.h
+//  Long Play
+//
+//  Created by Ash Furrow on 2013-01-25.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LPAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ LongPlay/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Long Play
+//
+//  Created by Ash Furrow on 2013-01-25.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
